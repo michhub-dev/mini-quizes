ced115fbfc291aaedd3b18fa4d74b3738b8043b3
Michelle David <57120467+michhub-dev@users.noreply.github.com>
2021-10-06 01:15:02 +0100

    First commit. Third quiz

---
+++ switch-st.js
@@ -0,0 +1,32 @@
+/* Write a switch statement to set the average salary of a person based on their type of completed education.*/
+
+
+
+let education ="no high school diploma";
+ let salary = 25636.80;
+ let message; 
+
+ switch(salary){
+      case 25636.80: 
+        message+= "in 2015, a person with " + education + " " + "earned an average of " + "$" + salary.toLocaleString("en-US") + " " + "/year";
+        break;
+     case 35256: 
+        message+= "in 2015, a person with " + education + " " + "earned an average of " + "$" + salary.toLocaleString("en-US") + " " + "/year";
+        break;
+     case 41496: 
+        message+= "in 2015, a person with " + education + " " + "earned an average of " + "$" + salary.toLocaleString("en-US") + " " + "/year";
+        break;
+     case 59124: 
+        message+= "in 2015, a person with " + education + " " + "earned an average of " + "$" + salary.toLocaleString("en-US") + " " + "/year";
+        break;
+     case 69732: 
+        message+= "in 2015, a person with " + education + " " + "earned an average of " + "$" + salary.toLocaleString("en-US") + " " + "/year";
+        break;
+     case 89960: 
+        message+= "in 2015, a person with " + education + " " + "earned an average of " + "$" + salary.toLocaleString("en-US") + " " + "/year";
+        break;
+     case 84396: 
+        message+= "in 2015, a person with " + education + " " + "earned an average of " + "$" + salary.toLocaleString("en-US") + " " + "/year";
+
+ };
+ console.log(message)
